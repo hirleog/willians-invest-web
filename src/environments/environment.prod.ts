63e1afbccee854e96e9e34b0077d760ea512b048
Guilherme Hirle <guilherme.hirle1@gmail.com>
2025-05-06 19:08:05 -0300

    ajuste de https para requisições

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://89.116.73.70:3000', // URL da API do backend
+  apiUrl: 'https://89.116.73.70:3000', // URL da API do backend
   acess_token: '',
   adminKey: '28d4639e283f488d36b2ae20ca3e28ce62ba9a47374c76a3180317b7d865e55a', // chave secreta para o dashboard
 };
